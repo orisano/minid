ba10757d427d4b2e2fd9a43e578e549431b4efd7
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2019-12-16 18:50:36 +0900

    refactor: split function

--- main.go
+++ main.go
@@ -13,10 +13,154 @@ import (
 	"unicode"
 
 	"github.com/moby/buildkit/frontend/dockerfile/parser"
+	"golang.org/x/xerrors"
 )
 
-type entry struct {
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("minid: ")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type flags struct {
+	dockerfilePath string
+	outputPath     string
+}
+
+type envEntry struct {
 	key, value string
+}
+
+func run() error {
+	var flags flags
+	flag.StringVar(&flags.dockerfilePath, "f", "Dockerfile", "Dockerfile's path")
+	flag.StringVar(&flags.outputPath, "o", "-", "generated Dockerfile path")
+	flag.Parse()
+
+	dockerfile, err := readDockerfile(flags.dockerfilePath)
+	if err != nil {
+		return xerrors.Errorf("read Dockerfile: %w", err)
+	}
+
+	wc, err := openWriter(flags.outputPath)
+	if err != nil {
+		return xerrors.Errorf("open Dockerfile writer: %w", err)
+	}
+	defer wc.Close()
+
+	if err := writeMinifiedDockerfile(wc, dockerfile); err != nil {
+		return xerrors.Errorf("minify: %w", err)
+	}
+
+	return nil
+}
+
+func openReader(name string) (io.ReadCloser, error) {
+	if name == "-" {
+		return os.Stdin, nil
+	}
+	return os.Open(name)
+}
+
+func openWriter(name string) (io.WriteCloser, error) {
+	if name == "-" {
+		return os.Stdout, nil
+	}
+	return os.Create(name)
+}
+
+func readDockerfile(dockerfilePath string) ([]byte, error) {
+	rc, err := openReader(dockerfilePath)
+	if err != nil {
+		return nil, xerrors.Errorf("open reader: %w", err)
+	}
+	defer rc.Close()
+	return ioutil.ReadAll(rc)
+}
+
+func writeMinifiedDockerfile(w io.Writer, dockerfile []byte) error {
+	head, _, err := bufio.NewReader(bytes.NewReader(dockerfile)).ReadLine()
+	if err != nil {
+		return xerrors.Errorf("head Dockerfile: %w", err)
+	}
+
+	result, err := parser.Parse(bytes.NewReader(dockerfile))
+	if err != nil {
+		return xerrors.Errorf("parse: %w", err)
+	}
+
+	bw := bufio.NewWriter(w)
+	if bytes.HasPrefix(head, []byte("# syntax =")) {
+		fmt.Fprintln(bw, string(head))
+	}
+	for _, nodes := range compressBy(result.AST.Children, isSameCommand) {
+		switch cmd := strings.ToUpper(nodes[0].Value); cmd {
+		case "RUN":
+			fmt.Fprint(bw, nodes[0].Original)
+			for _, node := range nodes[1:] {
+				fmt.Fprint(bw, " && ", node.Next.Value)
+			}
+			fmt.Fprintln(bw)
+		case "ENV":
+			var entries []envEntry
+			for _, node := range nodes {
+				for n := node.Next; n != nil; n = n.Next.Next {
+					key := n.Value
+					val := n.Next.Value
+					entries = append(entries, envEntry{key: key, value: val})
+				}
+			}
+
+			for len(entries) > 0 {
+				fmt.Fprint(bw, cmd)
+				vars := map[string]bool{}
+				for len(entries) > 0 {
+					e := entries[0]
+					if hasReference(vars, e.value) {
+						break
+					}
+					vars[e.key] = true
+					fmt.Fprint(bw, " ", e.key, "=", e.value)
+					entries = entries[1:]
+				}
+				fmt.Fprintln(bw)
+			}
+		case "LABEL":
+			fmt.Fprint(bw, cmd)
+			for _, node := range nodes {
+				for n := node.Next; n != nil; n = n.Next.Next {
+					key := n.Value
+					val := n.Next.Value
+					fmt.Fprint(bw, " ", key, "=", val)
+				}
+			}
+			fmt.Fprintln(bw)
+		case "ADD", "COPY":
+			for _, xs := range compressBy(nodes, isSameDestination) {
+				fmt.Fprint(bw, cmd)
+				if len(xs[0].Flags) > 0 {
+					fmt.Fprint(bw, " ", strings.Join(xs[0].Flags, " "))
+				}
+				for _, x := range xs {
+					for n := x.Next; n.Next != nil; n = n.Next {
+						fmt.Fprint(bw, " ", n.Value)
+					}
+				}
+				fmt.Fprint(bw, " ", destination(xs[0]))
+				fmt.Fprintln(bw)
+			}
+		default:
+			for _, node := range nodes {
+				fmt.Fprintln(bw, node.Original)
+			}
+		}
+	}
+	if err := bw.Flush(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func compressBy(nodes []*parser.Node, pred func(a, b *parser.Node) bool) [][]*parser.Node {
@@ -54,114 +198,6 @@ func isSameDestination(a, b *parser.Node) bool {
 		return false
 	}
 	return destination(a) == destination(b)
-}
-
-func main() {
-	var dockerfilePath string
-	flag.StringVar(&dockerfilePath, "f", "Dockerfile", "Dockerfile's path")
-	outputPath := flag.String("o", "-", "generated Dockerfile path")
-	flag.Parse()
-
-	var r io.Reader
-	if dockerfilePath == "-" {
-		r = os.Stdin
-	} else {
-		f, err := os.Open(dockerfilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-		r = f
-	}
-	var w io.Writer
-	if *outputPath == "-" {
-		w = os.Stdout
-	} else {
-		f, err := os.Create(*outputPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-		w = f
-	}
-
-	b, err := ioutil.ReadAll(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	head, _, err := bufio.NewReader(bytes.NewReader(b)).ReadLine()
-	if err != nil {
-		log.Fatal(err)
-	}
-	result, err := parser.Parse(bytes.NewReader(b))
-	if err != nil {
-		log.Fatal(err)
-	}
-	if bytes.HasPrefix(head, []byte("# syntax =")) {
-		fmt.Fprintln(w, string(head))
-	}
-	for _, nodes := range compressBy(result.AST.Children, isSameCommand) {
-		switch cmd := strings.ToUpper(nodes[0].Value); cmd {
-		case "RUN":
-			fmt.Fprint(w, nodes[0].Original)
-			for _, node := range nodes[1:] {
-				fmt.Fprint(w, " && ", node.Next.Value)
-			}
-			fmt.Fprintln(w)
-		case "ENV":
-			var entries []entry
-			for _, node := range nodes {
-				for n := node.Next; n != nil; n = n.Next.Next {
-					key := n.Value
-					val := n.Next.Value
-					entries = append(entries, entry{key: key, value: val})
-				}
-			}
-
-			for len(entries) > 0 {
-				fmt.Fprint(w, cmd)
-				vars := map[string]bool{}
-				for len(entries) > 0 {
-					e := entries[0]
-					if hasReference(vars, e.value) {
-						break
-					}
-					vars[e.key] = true
-					fmt.Fprint(w, " ", e.key, "=", e.value)
-					entries = entries[1:]
-				}
-				fmt.Fprintln(w)
-			}
-		case "LABEL":
-			fmt.Fprint(w, cmd)
-			for _, node := range nodes {
-				for n := node.Next; n != nil; n = n.Next.Next {
-					key := n.Value
-					val := n.Next.Value
-					fmt.Fprint(w, " ", key, "=", val)
-				}
-			}
-			fmt.Fprintln(w)
-		case "ADD", "COPY":
-			for _, xs := range compressBy(nodes, isSameDestination) {
-				fmt.Fprint(w, cmd)
-				if len(xs[0].Flags) > 0 {
-					fmt.Fprint(w, " ", strings.Join(xs[0].Flags, " "))
-				}
-				for _, x := range xs {
-					for n := x.Next; n.Next != nil; n = n.Next {
-						fmt.Fprint(w, " ", n.Value)
-					}
-				}
-				fmt.Fprintln(w, " ", destination(xs[0]))
-			}
-		default:
-			for _, node := range nodes {
-				fmt.Fprintln(w, node.Original)
-			}
-		}
-	}
 }
 
 func hasReference(vars map[string]bool, expr string) bool {
